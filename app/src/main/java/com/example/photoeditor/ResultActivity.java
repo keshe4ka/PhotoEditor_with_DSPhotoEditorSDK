09a96800d416d93d07b9ed3588621e53b0b9438e
keshe4ka <artem.anaschenko@gmail.com>
2021-05-23 20:40:41 +0300

    Кнопка Home на ResultActivity теперь работает, еще работает изображение в ResultActivity

--- app/src/main/java/com/example/photoeditor/ResultActivity.java
+++ app/src/main/java/com/example/photoeditor/ResultActivity.java
@@ -2,7 +2,9 @@ package com.example.photoeditor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.photoeditor.databinding.ActivityResultBinding;
 
@@ -14,8 +16,18 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityResultBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_result);
+        setContentView(binding.getRoot());
 
         binding.image.setImageURI(getIntent().getData());
+
+        binding.homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+        );
     }
 }
